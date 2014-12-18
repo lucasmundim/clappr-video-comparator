634f33b89d885fce276820e3625d53a143d51bfb
Lucas Mundim <lucas.mundim@corp.globo.com>
2014-12-18 17:50:28 -0200

    Add videos.js

---
+++ videos.js
@@ -0,0 +1,13 @@
+var videos = {
+  // == Examples:
+  // === Local File
+  // "big_buck_bunny": [
+  //   "data/big_buck_bunny/trailer_480p.mp4",
+  //   "data/big_buck_bunny/trailer_720p.mp4"
+  // ],
+  // === Remote file
+  "sintel": [
+    "https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4",
+    "https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4"
+  ]
+}
